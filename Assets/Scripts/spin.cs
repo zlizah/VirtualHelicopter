4fefe3a838371b03bfc2ff702eb950ff40652f78
Seymourr <jack_shabo6@hotmail.com>
2016-05-29 19:32:44 +0200

    Final mod setting on spin..

--- Assets/Scripts/spin.cs
+++ Assets/Scripts/spin.cs
@@ -5,8 +5,13 @@ public class spin : MonoBehaviour {
 
 	public Vector3 spinRotor(float w, float thrust) {
 		float revpersec = w / (2 * Mathf.PI);
-		revpersec *= Mathf.Rad2Deg * 0.3f; //Scaling for animation..
-		revpersec *= (thrust/100);
+		revpersec *= Mathf.Rad2Deg * 0.3f; //Scaling for animation.. (Modification)
+
+		if (thrust != 0) {
+			revpersec *= (thrust / 100); //Slight modification
+		}
+
+		print ("REVVING: " + revpersec);
 		return new Vector3 (0, 0, -revpersec);
 	}
 }
