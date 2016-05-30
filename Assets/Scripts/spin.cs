c40d3cd0b94848b5e6edd9f605c0fa83ecd619bb
Seymourr <jack_shabo6@hotmail.com>
2016-05-30 23:47:25 +0200

    Rotor spin increase and option for basic 3df cam added

--- Assets/Scripts/spin.cs
+++ Assets/Scripts/spin.cs
@@ -5,8 +5,8 @@ public class spin : MonoBehaviour {
 
 	public Vector3 spinRotor(float w, float thrust) {
 		float revpersec = w / (2 * Mathf.PI);
-		revpersec *= Mathf.Rad2Deg * 0.3f; //Scaling for animation.. (Modification)
-
+		revpersec *= Mathf.Rad2Deg * 0.4f; //Scaling for animation.. (Modification)
+	
 
 
 		return new Vector3 (0, 0, -revpersec);
